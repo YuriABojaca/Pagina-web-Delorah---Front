5da9bd656e62a9a08216fbee3b6bda88bae04691
YuriABojaca <yuri.al26@hotmail.com>
2024-11-30 09:13:21 -0500

    Primer commit
    Primer commit

---
+++ js/contact-form.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contact-form');
+    const response_div = document.getElementById('response');
+  
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault(); // Previene el envío tradicional del formulario
+  
+      // Capturar los datos del formulario
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const message = document.getElementById('message').value;
+  
+      try {
+        // Enviar los datos al servidor
+        const response = await fetch('http://localhost:5001/api/contact-form', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, email, message }),
+        });
+  
+        if (response.ok) {
+          // Mostrar mensaje de éxito
+          response_div.textContent = '¡Gracias por tu mensaje! Te contactaremos pronto.';
+          response_div.style.color = 'black';
+          form.reset(); // Opcional: Limpia el formulario
+        } else {
+          // Manejo de errores
+          const error = await response.json();
+          response_div.textContent = `Error: ${error.message || 'No se pudo enviar el mensaje.'}`;
+          response_div.style.color = 'red';
+        }
+      } catch (error) {
+        console.error(error);
+        response_div.textContent = 'Ocurrió un error al enviar el mensaje.';
+        response_div.style.color = 'red';
+      }
+    });
+  });
+  
---
+++ js/product.js
@@ -0,0 +1,27 @@
+async function obtenerProductos() {
+    const params = new URLSearchParams(window.location.search);
+    const category = params.get('category'); // Ejemplo: 'blusas'
+    console.log(`Categoría seleccionada: ${category}`);
+    try {
+      const response = await fetch(`http://localhost:5001/api/products?category=${category}`);
+      const productos = await response.json();
+  
+      const contenedor = document.getElementById('gallery-section');
+      contenedor.innerHTML = ''; // Limpiar productos existentes
+  
+      productos.forEach((product) => {
+        const productDiv = document.createElement('div');
+        productDiv.className="product-item";
+        productDiv.innerHTML = `
+          <h3>${product.name}</h3>
+          <img src="${product.urlimage}" alt="${product.name}" width="200">
+        `;
+        contenedor.appendChild(productDiv);
+      });
+    } catch (error) {
+      console.error('Error al obtener productos:', error);
+    }
+  }
+  
+  // Llamar a la función con una categoría de ejemplo
+  obtenerProductos();
